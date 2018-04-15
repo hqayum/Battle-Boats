2250fa1cc1d515ef2ded0023f04916149a2e2c6b
Hasson Qayum <hqayum@yahoo.com>
2018-04-14 17:23:03 -0700

    Updates to Test file, tests all protocol func

--- Code/Tester.c
+++ Code/Tester.c
@@ -1,8 +1,19 @@
+/* 
+ * File:   Tester.c
+ * Author: Hasson Qayum
+ * Collaborator: Kameron Gill
+ */
+// To test thoroughly, uncomment printf statements in Protocol.c
+
 // **** Include libraries here ****
 // Standard libraries
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 //CMPE13 Support Library
 #include "BOARD.h"
+#include "CircularBuffer.h"
 
 // Microchip libraries
 #include <xc.h>
@@ -11,6 +22,7 @@
 // User libraries
 #include "Buttons.h"
 #include "Oled.h"
+#include "Protocol.h"
 
 // **** Set any macros or preprocessor directives here ****
 
@@ -43,18 +55,103 @@ int main()
 
     ButtonsInit();
 
-    OledInit();
+//    OledInit();
+//
+//    // Prompt the user to start the game and block until the first character press.
+//    OledDrawString("Press BTN4 to start.");
+//    OledUpdate();
+//    while ((buttonEvents & BUTTON_EVENT_4UP) == 0);
 
-    // Prompt the user to start the game and block until the first character press.
-    OledDrawString("Press BTN4 to start.");
-    OledUpdate();
-    while ((buttonEvents & BUTTON_EVENT_4UP) == 0);
 
+    /******************************************************************************
+     * Your code goes in between this comment and the following one with asterisks.
+     *****************************************************************************/
+    
+    char *message;
+    message = malloc(PROTOCOL_MAX_MESSAGE_LEN);
+    
+    // Initiate Guess Data
+    GuessData GDTest;
+    GDTest.row = 553487;
+    GDTest.col = 30023;
+    GDTest.hit = 1111111;
+    
 
-/******************************************************************************
- * Your code goes in between this comment and the following one with asterisks.
- *****************************************************************************/
-
+//    GDTest.row = 57;
+//    GDTest.col = 1;
+//    GDTest.hit = 1111;
+    
+    // Test Encode Guess messages
+    ProtocolEncodeCooMessage(message, &GDTest);
+    printf("%s", message);
+    ProtocolEncodeHitMessage(message, &GDTest);
+    printf("%s", message);
+    
+    
+    // Initiate Negotiation Data
+    NegotiationData NDTest;
+    ProtocolGenerateNegotiationData(&NDTest);
+    
+    // Check values of generated Negotiation Data
+    printf("%d\t", NDTest.encryptedGuess);
+    printf("%d\t", NDTest.encryptionKey);
+    printf("%d\t", NDTest.guess);
+    printf("%d\n", NDTest.hash);
+    
+    // Test Encode Negotiation Messages
+    ProtocolEncodeChaMessage(message, &NDTest);
+    printf("%s", message);
+    ProtocolEncodeDetMessage(message, &NDTest);
+    printf("%s", message);
+    
+    
+    // Manual test, hard-coded
+//    ProtocolEncodeCooMessage(message, &GDTest);
+//    ProtocolEncodeHitMessage(message, &GDTest);
+//    ProtocolEncodeChaMessage(message, &NDTest);
+    ProtocolEncodeDetMessage(message, &NDTest);
+    //message = "$DET,222,43*F7\n";
+    printf("\n%s\n\n", message);
+    
+    
+    // Test Decode and make sure FSM is working properly
+    NegotiationData nData;
+    GuessData gData;
+    int iteration = 0;
+    ProtocolParserStatus decodeReturn;
+    while(iteration < strlen(message)) {
+        decodeReturn = ProtocolDecode(message[iteration], &nData, &gData);
+        if (decodeReturn == PROTOCOL_PARSING_FAILURE) {
+            printf("ERROR!!!!\n");
+        } else if (decodeReturn == PROTOCOL_PARSED_DET_MESSAGE) {
+            printf("returned DET\n");
+        } else if (decodeReturn == PROTOCOL_PARSED_HIT_MESSAGE) {
+            printf("returned HIT\n");
+        } else if (decodeReturn == PROTOCOL_PARSED_COO_MESSAGE) {
+            printf("return COO\n");
+        } else if (decodeReturn == PROTOCOL_PARSED_CHA_MESSAGE) {
+            printf("return CHA\n");
+        }
+        iteration++;
+    }
+    
+   // Test PVND function
+    if (ProtocolValidateNegotiationData(&NDTest) == SUCCESS) {
+        printf("PVND successful\n");
+    } else {
+        printf("PVND failed\n");
+    }
+    
+    // Test TurnOrder function
+    NegotiationData TurnOrderTest;
+    ProtocolGenerateNegotiationData(&TurnOrderTest);
+    if(ProtocolGetTurnOrder(&NDTest, &TurnOrderTest) == TURN_ORDER_START){
+        printf("I Start\n");
+    } else if(ProtocolGetTurnOrder(&NDTest, &TurnOrderTest) == TURN_ORDER_DEFER){
+        printf("They Start\n");
+    } else { // if(ProtocolGetTurnOrder(&NDTest, &TurnOrderTest) == TURN_ORDER_TIE)
+        printf("TIE\n");
+    }
 
 /******************************************************************************
  * Your code goes in between this comment and the preceeding one with asterisks
